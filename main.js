e956f27c5366f4993e3c198878608bdedc0d9c93
AlaaCool25 <97164012+AlaaCool25@users.noreply.github.com>
2022-12-29 20:47:48 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+objects = [];
 video = "";
 status = "";
 
@@ -13,6 +14,22 @@ function setup() {
 
 function draw() {
     image(video, 0, 0, 480, 380);
+    if (status != "") 
+    {
+        objectDetector.detect(video, gotResult);
+        for (i  = 0; i < objects.length; i++) 
+        {
+            document.getElementById("status").innerHTML = "Status : Objects Detected";
+            document.getElementById("number_of_object").innerHTML = "Number Of Objects Are : " + objects.length;
+
+            fill('#fc77d4');
+            percent = floor(objects[i].confidence * 100);
+            text(objects[i].label + " " + percent + "%", object[i].x + 15, object[i].y + 15);
+            noFill();
+            stroke('#fc77d4');
+            rect(object[i].x + 15, object[i].y + 15, object[i].width + 15, object[i].height + 15);
+        }
+    }
 }
 
 function start() {
@@ -26,4 +43,17 @@ function modelLoaded() {
     video.loop();
     video.speed(1);
     video.volume(1);
+}
+
+function gotResult(error, results) 
+{
+    if (error) 
+    {
+        console.log(error);
+    } 
+    else 
+    {
+        console.log(results);
+        objects = results;
+    }
 }
